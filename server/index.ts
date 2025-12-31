79ff7af41175a8aad77e26d00ad5927f46987bf5
Emmanuel Akinola <emmanuelakinola255@gmail.com>
2025-12-31 18:49:23 +0100

    Deployment test 02

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,7 @@ app.get('/health', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
